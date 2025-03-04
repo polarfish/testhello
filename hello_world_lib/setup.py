00e66a0f71b92c3721e8b07dda1bea83b907dad1
Eugene Grybinnyk <eugene.grybinnyk@gmail.com>
2025-03-04 17:49:57 +0100

    Add files via upload

---
+++ hello_world_lib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hello_world_lib',
+    version='0.1',
+    py_modules=['hello'],
+    description='A simple Hello World library',
+    author='John Lee',
+    author_email='johnlee@example.com',
+    url='https://github.com/polarfish/hello_world_lib',
+)
