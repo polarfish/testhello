48037bb936a0695fc46ac8785adab4b38af6106c
Eugene Grybinnyk <eugeneg@backbase.com>
2025-03-04 18:22:33 +0100

    test default src layout

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
-from setuptools import find_packages
 
 setup(
-    packages=find_packages(),
     name='hello_world_lib',
     version='0.1',
-    py_modules=['hello'],
     description='A simple Hello World library',
     author='John Lee',
     author_email='johnlee@example.com',
